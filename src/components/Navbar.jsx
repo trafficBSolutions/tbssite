efe8c79f846bac5f482ca609b2979b783712c1d7
trafficBSolutions <121638756+trafficBSolutions@users.noreply.github.com>
2024-03-18 11:52:22 -0400

    Delete src directory

--- src/components/Navbar.jsx
+++
@@ -1,9 +0,0 @@
-import {Link} from 'react-router-dom'
-
-export default function Navbar() {
-    <nav>
-        <Link to='/'>Home</Link>
-        <Link to='/applynow'>Apply Now</Link>
-        <Link to='/trafficcontrol'>Traffic Control</Link>
-        </nav>
-}
--- src/constants/constantapi.js
+++
@@ -1,2 +0,0 @@
-export const REACT_APP_GOOGLE_MAPS_API_KEY = 
-"AIzaSyCSg4cRTt5eU31O_fqoERu4JYTe9xE0Zk0";
